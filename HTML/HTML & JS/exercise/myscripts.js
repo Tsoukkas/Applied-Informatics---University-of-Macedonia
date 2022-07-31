5cffdc521f7b3a7d9e0e207b92940d5f4b79b5a0
Andonis Tsoukkas <antonistsoukkas@gmail.com>
2022-07-31 11:53:17 +0300

    [

--- HTML/HTML & JS/exercise/myscripts.js
+++
@@ -1,7 +0,0 @@
-<script>
-    <button if="yelBack" onclick="MyFunction()">
-    </button>
-    Function MyFunction(){
-       let button = document.querySelectorall[0].innerHTML.style.backgroundcolor = "yellow";
- };
-</script>
